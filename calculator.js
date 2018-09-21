e5d895552b1b716fdb226f5408a5804e61be30ce
Rebecca Viner <rtviner@gmail.com>
2018-09-21 16:05:09 -0600

    add returns to operate function

--- calculator.js
+++ calculator.js
@@ -20,16 +20,16 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
 	switch (operator) {
 		case "+":
-			add(num1, num2);
+			return add(num1, num2);
 			break;
 		case "-":
-			subtract(num1, num2);
+			return subtract(num1, num2);
 			break;
 		case "*":
-			multiply(num1, num2);
+			return multiply(num1, num2);
 			break;
 		case "/":
-			divide(num1, num2);
+			return divide(num1, num2);
 			break;
 	}
 }
