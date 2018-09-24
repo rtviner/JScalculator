f8635b69b1bf1faea27809a559fdd2fb31e1005d
Rebecca Viner <rtviner@gmail.com>
2018-09-24 11:35:40 -0600

    add output function to display numbers clicked in output box

--- calculator.js
+++ calculator.js
@@ -1,4 +1,6 @@
-let result;
+let outputValue = document.getElementById('output');
+let numbers = document.querySelectorAll('.num-btn');
+let inputValue = [];
 
 
 function add(num1, num2) {
@@ -17,6 +19,10 @@ function divide(num1, num2) {
 	return num1 / num2;
 }
 
+function sqrt(num1) {
+	Math.sqrt(num1);
+}
+
 function operate(operator, num1, num2) {
 	switch (operator) {
 		case "+":
@@ -31,5 +37,32 @@ function operate(operator, num1, num2) {
 		case "/":
 			return divide(num1, num2);
 			break;
+		case "sqrt":
+			return sqrt(num1);
+			break;
 	}
 }
+
+numbers.forEach((number) => {
+	number.addEventListener('click', output);
+});
+
+function output(event) {
+	inputValue.push(event.target.innerHTML);
+	outputValue.innerHTML = inputValue.join("");
+	console.log(inputValue);
+	return outputValue;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
