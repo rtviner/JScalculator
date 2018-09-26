98caa900a053f94b820428511fcda26a2727ce53
Rebecca Viner <rtviner@gmail.com>
2018-09-26 10:31:59 -0600

    make outputReg a global variable

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ let operators = document.querySelectorAll('.op-btn');
 let equals = document.getElementById('equal-btn');
 let clear = document.getElementById('clear');
 let inputValue = [];
+let outputReg = /(\d+\.*\d*)([+/*-]?)(\d*\.*\d*)/;
 
 function add(num1, num2) {
 	return num1 + num2;
@@ -26,29 +27,42 @@ function sqrt(num1) {
 	Math.sqrt(num1);
 }
 
-// function operate(operator, num1, num2) {
+function operate() {
 
-// 	num1 = parseFloat(num1);
-// 	num2 = parseFloat(num2);
+	console.log(inputValue);
+	let inputString = inputValue.join("");
+	console.log(inputString);
+	let match = outputReg.exec(inputString);
 
-// 	switch (operator) {
-// 		case "+":
-// 			outputValue.innerHTML = add(num1, num2);
-// 			break;
-// 		case "-":
-// 			outputValue.innerHTML = subtract(num1, num2);
-// 			break;
-// 		case "*":
-// 			outputValue.innerHTML = multiply(num1, num2);
-// 			break;
-// 		case "/":
-// 			outputValue.innerHTML = divide(num1, num2);
-// 			break;
-// 		case "sqrt":
-// 			outputValue.innerHTML = sqrt(num1);
-// 			break;
-// 	}
-// }
+	let num1 = match[1];
+	let operator = match[2];
+	let num2 = match[3];
+
+	console.log("event:", event);
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	console.log("operator:", operator);
+	console.log("num1:", num1);
+	console.log("num2:", num2);
+
+	switch (operator) {
+		case "+":
+			outputValue.innerHTML = add(num1, num2);
+			break;
+		case "-":
+			outputValue.innerHTML = subtract(num1, num2);
+			break;
+		case "*":
+			outputValue.innerHTML = multiply(num1, num2);
+			break;
+		case "/":
+			outputValue.innerHTML = divide(num1, num2);
+			break;
+		case "sqrt":
+			outputValue.innerHTML = sqrt(num1);
+			break;
+	}
+}
 
 numbers.forEach((number) => {
 	number.addEventListener('click', output);
@@ -63,63 +77,25 @@ operators.forEach((operator) => {
 
 
 function output(event) {
-	
+
 	inputValue.push(event.target.innerHTML);
 
 	if (inputValue.indexOf('.') > -1) decimal.removeEventListener('click', output);
 
-	let reg = /(\d+\.*\d*)([+/*-]?)(\d*\.*\d*)/;
-
 	let inputString = inputValue.join("");
-	let match = reg.exec(inputString);
+	let match = outputReg.exec(inputString);
 
 	let num1 = match[1];
 	let operator = match[2];
 	let num2 = match[3];
 
 	outputValue.innerHTML = num1 + ' ' + operator + ' ' + num2;
+
+	// if (test)
+
+
 	
-	// equals.addEventListener('click', operate(operator, num1, num2));
- 
 }
 
-// function addOperator(event) {
-//     inputValue.push(" ",event.target.innerHTML," ");
-//     	console.log(inputValue);
 
-// 	numbers.forEach((number) => {
-// 		number.addEventListener('click', output);
-// 	});
-
-// 	decimal.addEventListener('click', output);
-// }
-    
-
-// function buildEquation(inputValue) {
-// 	    let inputValueStr = inputValue.join("");
-
-// 		let reg = /(\d+)([+/*-])(\d+)/;
-// 		let match = reg.exec(inputValueStr);
-
-// 		let num1 = match[1];
-// 		let operator = match[2];
-// 		let num2 = match[3];
-
-// 		outputValue.innerHTML = num1 + ' ' + operator + ' ' + num2;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+equals.addEventListener('click', operate);
