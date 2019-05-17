104e6749e610e0bce3c0d81c8f3e2ee6247a1eb2
Rebecca Viner <rtviner@gmail.com>
2019-05-16 20:52:00 -0600

    add eNotation function to convert answers longer than 20 characters to scientific e notation, add lastNum function which returns lastNum of outputHTML, and filterLastNum which determines whether or not filter character is present in last num, and reduce decimalFilter functiont to one if statement

--- calculator.js
+++ calculator.js
@@ -15,10 +15,10 @@ var calculator = (function () {
 	window.addEventListener('keydown', keyFilter) 
 
 	function keyFilter(event) {
-		if (/\d/.test(event.key) === true) {
+		if (/\d/.test(event.key)) {
 			input(event);
 		}
-		else if (/[√+/*-]/.test(event.key) === true) {
+		else if (/[√+/*-]/.test(event.key)) {
 			operatorFilter(event);
 		}
 		else if (event.key === "Backspace") {
@@ -55,27 +55,27 @@ var calculator = (function () {
 
 	answerBtn.addEventListener('click', answerFilter);
 
+	const lastNum = (string) => {
+		let outputArray = string.split(" ");
+		return outputArray[outputArray.length -1];
+	}
+
+	const filterLastNum = (string, filterChar) => string.indexOf(filterChar) === -1;
+
 	function decimalFilter(event) {
-		// test if there is a decimal at all
-		if (/\.+/.test(outputValue.innerHTML) === false) {
-			input(event);
-		}
-		// grab the last number of the string and check that number for a decimal
-		else {
-			let lastNum = outputValue.innerHTML.replace(/\d*\.*\d*\s[-+/*]\s/g, "");
-				if (lastNum.indexOf(".") === -1) {
-					input(event);
-				}
+		if  (filterLastNum(lastNum(outputValue.innerHTML), ".") || outputValue.innerHTML === answerBtn.value) {
+				input(event);
 		}
 	}
 
 	function operatorFilter(event) {
 		let sqrtEquationReg = /\d*\.?\d+\s{1}\√+/;
 		// if there is a number in the output html and the last input was not an operator allow an operator in the output
-		if (/\d/.test(outputValue.innerHTML) === true && /[√+/*-]/.test(outputValue.innerHTML[outputValue.innerHTML.length - 1]) === false) {
+	
+		if (/\d/.test(outputValue.innerHTML) && outputValue.innerHTML[outputValue.innerHTML.length - 1] !== " ") {
 			input(event);
 		}
-		if (sqrtEquationReg.test(outputValue.innerHTML) === true) {
+		if (sqrtEquationReg.test(outputValue.innerHTML)) {
 			let inputString = outputValue.innerHTML;
 			calculate(inputString);
 		}
@@ -83,7 +83,7 @@ var calculator = (function () {
 
 	function equalsFilter(event) {
 		// if there is an operator with 0 or 1 decimals and 1 or more digits call the calculate formula
-		if (/[+/*-]{1}\s\d*\.?\d+/g.test(outputValue.innerHTML) === true) {
+		if (/[+/*-]{1}\s\d*\.?\d+/g.test(outputValue.innerHTML)) {
 			let inputString = outputValue.innerHTML;
 			calculate(inputString);
 		}
@@ -95,9 +95,7 @@ var calculator = (function () {
 			input(event)
 		}
 		else {
-			let lastNum = outputValue.innerHTML.replace(/\d*\.*\d*\s[-+/*]\s/g, "");
-
-			if (lastNum.length === 0) {
+			if (lastNum(outputValue.innerHTML).length === 0) {
 				input(event);
 			}
 		}
@@ -124,17 +122,15 @@ var calculator = (function () {
 		pushOrClear(eventInput);
 
 		function pushOrClear(eventInput) {
-			if (/[√+/*-]/.test(eventInput) === true) {
-				if (inputValue.length >= 1 || inputValue[0] === "number") {
+			if (/[√+/*-]/.test(eventInput) && inputValue.length >= 1) {
 					eventInput = " " + eventInput + " ";
 					inputValue.push(eventInput);	
-				}
 			}			
-			else if (/[√+/*-]/.test(eventInput) === false) {
-				if (typeof inputValue[0] !== "number" || inputValue.length > 1)  {
+			else {
+				if (inputValue.length >= 1 && outputValue.innerHTML !== answerBtn.value) {
 					inputValue.push(eventInput);	
 				} 
-				else if (typeof inputValue[0] === "number" && /\d|\./.test(eventInput) === true) {
+				else if (/\d|\./.test(eventInput) === true) {
 					inputValue = [eventInput];
 				}
 			}
@@ -222,7 +218,6 @@ var calculator = (function () {
 
 	function decimalPlaces(num) {
 	    var match = (''+num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-	    if (!match) { return 0; }
 	    return Math.max(
 	        0,
 	        (match[1] ? match[1].length : 0)
@@ -237,12 +232,19 @@ var calculator = (function () {
 		return Math.sqrt(num1);
 	}
 
+	const eNotation = (string) => Number.parseFloat(string).toExponential(5);
+
 	function answer(inputString) {
 		let answer = inputString;
+		console.log("inputString:", inputString);
+		console.log("inputValue:", inputValue);
 
-	    if (answer.toString().length > 20) {
-	    	inputValue = [answer.toExponential()];
-	    	answerBtn.value = answer.toExponential();
+		console.log("is answer too long:", inputString.length > 20);
+		console.log(inputString.length);
+
+	    if (inputString.length > 20) {
+	    	inputValue = [eNotation(answer)];
+	    	answerBtn.value = eNotation(answer);
 	    }
 	    else {
 	    	inputValue = [answer];
