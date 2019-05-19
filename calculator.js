1359b8d1ded7f7cbe9cd2f3b2e2db11cfee7b4cf
Rebecca Viner <rtviner@gmail.com>
2019-05-19 14:26:48 -0600

    separate multiplyOrDivideEquation regex and, match, and replace into function makeMultiplyOrDivideEquation, and separate addOrSubtractEquation regex and match and replace into function makeAddorSubtractEquation to make things easier to follow, update parameters names to improve readability

--- calculator.js
+++ calculator.js
@@ -89,10 +89,11 @@ var calculator = (function () {
 	}
 
 	function sqRtFilter(event) {
-		//check if last number and outputValue are the same indicating output is just one number rather than an equation, if equation call equalsFilter to solve
+		//check if last number != outputValue indicating outputValue is at least 2 numbers and an operator, then call calculate to get answer for sqRt function
 		if (lastNum(outputValue.innerHTML) !== outputValue.innerHTML) {
-			equalsFilter(outputValue.innerHTML);
+			reduceByOrderOfOperations(outputValue.innerHTML);
 		}
+
 		calculateSqRt(outputValue.innerHTML);
 	}
 
@@ -103,133 +104,124 @@ var calculator = (function () {
 	}
 
 	function equalsFilter(event) {
-		console.log("equals Filter has been called with:", event)
-		// if there is an operator with 0 or 1 decimals and 1 or more digits call the calculate formula
-		if (/[+/*-]{1}\s\d*\.?\d+/g.test(outputValue.innerHTML)) {
-			let inputString = outputValue.innerHTML;
-			calculate(inputString);
+		//check if last number != outputValue indicating outputValue is at least 2 numbers and an operator, then call calculate to get answer
+		if (lastNum(outputValue.innerHTML) !== outputValue.innerHTML) {
+			reduceByOrderOfOperations(outputValue.innerHTML);
 		}
 	}
 
 	function backspace(event) {
 		const outputArray = [...outputValue.innerHTML];
-		const backspaceInput = (noOperator(lastNum(outputValue.innerHTML))) ? 
+		const backspacedInput = (noOperator(lastNum(outputValue.innerHTML))) ? 
 			outputArray.slice(0, outputArray.length - 1) :
 			outputArray.slice(0, outputArray.length - 3)	
-		output(backspaceInput);
+		output(backspacedInput);
 	};
 
 	function addValue(number) {
-		const inputValue = (outputValue.innerHTML === answerBtn.value) ? [number] : [...outputValue.innerHTML, number]
-		output(inputValue);		
+		const inputArray = (outputValue.innerHTML === answerBtn.value) ? [number] : [...outputValue.innerHTML, number]
+		output(inputArray);		
 	}
 
 	function addOperator(operator) {
 		let operatorString = " " + operator + " ";
-		const inputValue = [...outputValue.innerHTML, operatorString];
-		output(inputValue);
+		const inputArray = [...outputValue.innerHTML, operatorString];
+		output(inputArray);
 	}
 
-	function output(inputValue) {
-		outputValue.innerHTML = inputValue.join("");
+	function output(array) {
+		outputValue.innerHTML = array.join("");
 	}
 
-	function add(num1, num2) {
-		return num1 + num2;
+	function add(number1, number2) {
+		return number1 + number2;
 	}
 
-	function subtract(num1, num2) {
-		return num1 - num2;
+	function subtract(number1, number2) {
+		return number1 - number2;
 	}
 
-	function multiply(num1, num2) {
-		product = num1 * num2;
-		return parseFloat(product.toFixed(decimalPlaces(num1)+decimalPlaces(num2)));
+	function multiply(number1, number2) {
+		product = number1 * number2;
+		return parseFloat(product.toFixed(decimalPlaces(number1)+decimalPlaces(number2)));
 	}
 
-	function decimalPlaces(num) {
-	    var match = (''+num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+	function decimalPlaces(number) {
+	    var match = (''+number).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
 	    return Math.max(
 	        0,
 	        (match[1] ? match[1].length : 0)
 	        - (match[2] ? +match[2] : 0));
 	}
 
-	function divide(num1, num2) {
-		return num1 / num2;
+	function divide(number1, number2) {
+		return number1 / number2;
 	}
 
-	function sqrt(num1) {
-		return Math.sqrt(num1);
+	function sqrt(number) {
+		return Math.sqrt(number);
 	}
 
 	function calculateSqRt(number) {
-		console.log("calculateSqRt has been called with:", number);
 	    	answer(sqrt(number));
 	}
 
-	function calculate(inputString) {
-		console.log("calculate has been called:", inputString);
-		// if there are no operators (with spaces after them) left then return the answer
-		if (/[+\/*-]{1}\s/.test(inputString) === false) {
-	  		answer(inputString);
-	  	}
-	  //otherwise keep solving equations
-	  	else {
-	  		let regMD = /[\/\*]+/g; 
-	  		let regAS = /[+-]+/g; 
-	  		let outputRegMD = /(\-*\d*\.?\d+)\s{1}([*/]+)\s{1}(\-*\d*\.?\d+)/;
-	  		let outputRegAS = /(\-*\d*\.?\d+)\s{1}([+-]+)\s{1}(\-*\d*\.?\d+)/;
-	    //if there is a multiplication or division equation do those first
-			if (/[\/\*]+/.test(inputString) === true) {
-	        //assign the left and right sides of the equation and operator to variables;
-	    		let match = outputRegMD.exec(inputString);
-	      		let num1 = parseFloat(match[1]);
-	      		let operator = match[2];
-	      		let num2 = parseFloat(match[3]);
-	      
-	      		if (operator === "/") {
-	        // replace entire equation match with divide(num1, num2)
-	        		inputString = inputString.replace(outputRegMD, divide(num1, num2));
-	        		calculate(inputString);
-	      		}
-	      		else {
-	      //replace entire equation match with multiply(num1, num2)
-	        		inputString = inputString.replace(outputRegMD, multiply(num1, num2));
-	        		calculate(inputString);
-	      		}
-	    	}
-	    //if there is a addition or subtraction equation do that next
-	    	else if (/[+-]+/.test(inputString) === true) {
-	        //assign the left and right sides of the equation and operator to variables;
-	      		let match = outputRegAS.exec(inputString);
-	      		let num1 = parseFloat(match[1]);
-	      		let operator = match[2];
-	      		let num2 = parseFloat(match[3]);
-	  
-	      		if (operator === "+") {
-	        //replace entire equation match with add(num1, num2)
-	        		inputString = inputString.replace(outputRegAS, add(num1, num2));
-	        		calculate(inputString);
-	      		}
-	      		else {
-	         //replace entire equation match with subtract(num1, num2)
-	        		inputString = inputString.replace(outputRegAS, subtract(num1, num2));
-	        		calculate(inputString);
-	      		}
-	    	}
-	  	} 
+	function makeMultiplyOrDivideEquation(equationString) {
+		const multiplyOrDivideEquation = /(\-*\d*\.?\d+)\s{1}([*/]+)\s{1}(\-*\d*\.?\d+)/;
+
+	  	const match = multiplyOrDivideEquation.exec(equationString);
+	    const num1 = parseFloat(match[1]);
+	    const operator = match[2];
+	    const num2 = parseFloat(match[3]);
+	    //replace entire equation match with result of divide(num1, num2) or multiply(num1, num2) and call reduceByOrderOfOperations again
+	    const reducedEquationString = (operator === "/") ? 
+	    	equationString.replace(multiplyOrDivideEquation, divide(num1, num2)) :
+	    	equationString.replace(multiplyOrDivideEquation, multiply(num1, num2));
+
+	    reduceByOrderOfOperations(reducedEquationString);
 	}
 
-	const eNotation = (string) => Number.parseFloat(string).toExponential(5);
+	function makeAddOrSubtractEquation(equationString) {
+		const addOrSubtractEquation = /(\-*\d*\.?\d+)\s{1}([+-]+)\s{1}(\-*\d*\.?\d+)/;
 
-	function answer(inputString) {
-		console.log("answer has been called with:", inputString);
-	    const answer = (inputString.length > 20) ? eNotation(inputString) : inputString;
+		let match = addOrSubtractEquation.exec(equationString);
+	    let num1 = parseFloat(match[1]);
+	    let operator = match[2];
+	    let num2 = parseFloat(match[3]);
+	    //replace entire equation match with result of add(num1, num2) or subtract(num1, num2) and call reduceByOrderOfOperations again
+	    const reducedEquationString = (operator === "+") ?
+	    	equationString.replace(addOrSubtractEquation, add(num1, num2)) :
+	    	equationString.replace(addOrSubtractEquation, subtract(num1, num2));
 
-	    inputValue = [answer];
-	    answerBtn.value = answer;   
-	    output(inputValue);
+	    reduceByOrderOfOperations(reducedEquationString);
+	}
+
+	function reduceByOrderOfOperations(equationString) {
+		// if there are no operators (with spaces after them) left then return the answer
+		console.log("reduceByOrderOfOperations has been called with:", equationString)
+	
+		if (!/[+\/*-]{1}\s/.test(equationString)) {
+	  		answer(equationString);
+	  	}
+	  	//otherwise keep making and solving equations
+	  	if (/[*/]\s/.test(equationString)) {
+	  		console.log("calling makeMultiplyOrDivideEquation");
+	  		makeMultiplyOrDivideEquation(equationString);
+	  	}
+		else if (/[+-]\s/.test(equationString)) {
+			console.log("calling makeAddOrSubtractEquation");
+	  		makeAddOrSubtractEquation(equationString);
+	  	}
+ 
+	}
+
+	const eNotation = (numberString) => Number.parseFloat(numberString).toExponential(5);
+
+	function answer(numberString) {
+	    const answer = (numberString.length > 20) ? eNotation(numberString) : numberString;
+	    finalAnswer = [answer];
+	    answerBtn.value = finalAnswer;   
+	    output(finalAnswer);
 	}
 }());
 
