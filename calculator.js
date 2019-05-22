5ee2e4315748945cfa372e10a84e31b517d338c7
Rebecca Viner <rtviner@gmail.com>
2019-05-21 20:57:00 -0600

    clean up, remove trailing spaces, make sure all variables are declared with let or const

--- calculator.js
+++ calculator.js
@@ -6,10 +6,10 @@ var calculator = (function () {
     const outputValue = document.getElementById('display');
     const answerBtn = document.getElementById('answer');
 
-    const lastNum = (string) => { 
+    const lastNum = (string) => {
         let outputArray = string.split(" ");
-        return outputArray[outputArray.length -1];
-    }
+        return outputArray[outputArray.length - 1];
+    };
 
     window.addEventListener('keydown', (event) => {
         if (event.key === "Enter") {
@@ -24,9 +24,9 @@ var calculator = (function () {
         button.addEventListener('click', (event) => filter(event.target.innerHTML));
     });
 
-    function filter(event) { 
+    function filter(event) {
         if (event === "AC") {
-            newInputValue = [];
+            let newInputValue = [];
             return output(newInputValue);
         }
         if (event === "Backspace" || event === "Del") {
@@ -35,24 +35,28 @@ var calculator = (function () {
         if (/\d/.test(event)) {
             return addValue(event);
         }
-        if (event === "Ans" && lastNum(outputValue.innerHTML).length === 0) {
+        if (event === "Ans" && lastNum(outputValue.innerHTML)
+            .length === 0) {
             return addValue(answerBtn.value);
         }
-        if (event === "." && lastNum(outputValue.innerHTML).indexOf(".") === -1) {
+        if (event === "." && lastNum(outputValue.innerHTML)
+            .indexOf(".") === -1) {
             return addValue(event);
         }
-        if (lastNum(outputValue.innerHTML).length === 0 || lastNum(outputValue.innerHTML) === "." || outputValue.innerHTML === "ERROR") {
-            if (event === "√" || event === "=" || event === "Enter") {
+        if (lastNum(outputValue.innerHTML).length === 0 ||lastNum(outputValue.innerHTML) === "." ||outputValue.innerHTML === "ERROR") {
+            if (event === "√" || event === "=" ||
+                event === "Enter") {
                 return outputValue.innerHTML = "ERROR";
             }
         }
         if (event === "=" || event === "Enter") {
             return reduceEquations(outputValue.innerHTML);
         }
-        if (lastNum(outputValue.innerHTML).length !== 0 && lastNum(outputValue.innerHTML) !== "." && /[+/*-]/.test(event)) {
-            return addOperator(event); 
+        if (lastNum(outputValue.innerHTML).length !== 0 &&lastNum(outputValue.innerHTML) !== "." && /[+/*-]/.test(event)) {
+            return addOperator(event);
         }
-        if (lastNum(outputValue.innerHTML).length !== 0 && lastNum(outputValue.innerHTML) !== "." && event === "√") { 
+        if (lastNum(outputValue.innerHTML).length !== 0 &&lastNum(outputValue.innerHTML) !== "." &&
+            event === "√") {
             reduceEquations(outputValue.innerHTML);
             return calculateSqRt(outputValue.innerHTML);
         }
@@ -60,23 +64,24 @@ var calculator = (function () {
 
     function backspace(event) {
         const outputArray = [...outputValue.innerHTML];
-        const backspacedInput = (lastNum(outputValue.innerHTML).length > 0) ? 
+        const backspacedInput = (lastNum(outputValue.innerHTML).length > 0) ?
             outputArray.slice(0, outputArray.length - 1) :
-            outputArray.slice(0, outputArray.length - 3)	
+            outputArray.slice(0, outputArray.length - 3);
         output(backspacedInput);
     };
 
     function addValue(number) {
-        const inputArray = (outputValue.classList.contains("answer") || outputValue.innerHTML === "ERROR") ? [number] : [...outputValue.innerHTML, number];
+        const inputArray = (outputValue.classList.contains("answer") || outputValue.innerHTML === "ERROR") ?
+            [number] : [...outputValue.innerHTML, number];
         if (outputValue.classList.contains("answer")) {
             outputValue.classList.remove("answer");
         }
-        output(inputArray);		
+        output(inputArray);
     }
 
     function addOperator(operator) {
         let operatorString = " " + operator + " ";
-        const inputArray = [...outputValue.innerHTML, operatorString];
+        const inputArray = [...outputValue.innerHTML,operatorString];
         if (outputValue.classList.contains("answer")) {
             outputValue.classList.remove("answer");
         }
@@ -92,22 +97,23 @@ var calculator = (function () {
     const subtract = (number1, number2) => number1 - number2;
 
     const multiply = (number1, number2) => {
-        product = number1 * number2;
-        return parseFloat(product.toFixed(decimalPlaces(number1)+decimalPlaces(number2)));
-    }
+        let product = number1 * number2;
+        return parseFloat(product.toFixed(decimalPlaces(number1) +
+            decimalPlaces(number2)));
+    };
 
     function decimalPlaces (number) {
-        var match = (''+number).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+        let match = ('' + number).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
         return Math.max(
             0,
-            (match[1] ? match[1].length : 0)
-            - (match[2] ? +match[2] : 0));
+            (match[1] ? match[1].length : 0) -
+            (match[2] ? +match[2] : 0));
     }
 
     const divide = (number1, number2) => number1 / number2;
 
     const sqrt = (number) => Math.sqrt(number);
-    
+
     const calculateSqRt = (number) => answer(sqrt(number));
 
     const calculations = {
@@ -118,21 +124,21 @@ var calculator = (function () {
     };
 
     function reduceEquations (equationString) {
-        function makeEquation (expression, equationString) {
+        const makeEquation = (expression, equationString) => {
             const match = expression.exec(equationString);
             const num1 = parseFloat(match[1]);
             const operator = match[2];
             const num2 = parseFloat(match[3]);
 
-            let reducedEquationString = equationString.replace(expression, calculations[operator](num1, num2));
+            const reducedEquationString = equationString.replace(expression, calculations[operator](num1, num2));
             reduceEquations(reducedEquationString);
-        }
+        };
         // if there are no operators (with spaces after them) left then return the answer because there are no more equations to solve	
         if (!/[+\/*-]{1}\s/.test(equationString)) {
             return answer(equationString);
         }
         //otherwise keep making and solving equations
-        const expression = (/[*/]\s/.test(equationString)) ? 
+        const expression = (/[*/]\s/.test(equationString)) ?
             multiplicationOrDivision :
             additionOrSubtraction;
 
@@ -143,7 +149,7 @@ var calculator = (function () {
 
     function answer (numberString) {
         const answer = (numberString.length > 20) ? eNotation(numberString) : numberString;
-        finalAnswer = [answer];
+        let finalAnswer = [answer];
         answerBtn.value = finalAnswer;
         outputValue.classList.add("answer");
         output(finalAnswer);
