fd0455e87e5194d035a8401efa54b3e472a7a96a
Rebecca Viner <rtviner@gmail.com>
2019-05-22 15:47:35 -0600

    update regex expression for makeEquation to include decimals without following digits

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
-const multiplicationOrDivision = /(\-*\d*\.?\d+)\s{1}([*/]+)\s{1}(\-*\d*\.?\d+)/;
-const additionOrSubtraction = /(\-*\d*\.?\d+)\s{1}([+-]+)\s{1}(\-*\d*\.?\d+)/;
+const multiplicationOrDivision = /(\-*\d*\.?\d*)\s{1}([*/]+)\s{1}(\-*\d*\.?\d*)/;
+const additionOrSubtraction = /(\-*\d*\.?\d*)\s{1}([+-]+)\s{1}(\-*\d*\.?\d*)/;
 
 var calculator = (function () {
 
